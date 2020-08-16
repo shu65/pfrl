e996cca4c8a45639b19e49543262aafb7e5bcbb1
Shuji Suzuki <dolphinripple@gmail.com>
2020-08-17 08:28:02 +0900

    fix

--- pfrl/explorers/epsilon_greedy.py
+++ pfrl/explorers/epsilon_greedy.py
@@ -83,7 +83,7 @@ class LinearDecayEpsilonGreedy(explorer.Explorer):
         a, greedy = select_action_epsilon_greedily(
             self.epsilon, self.random_action_func, greedy_action_func
         )
-        print(f"pid {os.getpid()} select_action stack", traceback.format_stack)
+        print(f"pid {os.getpid()} select_action stack", traceback.format_stack())
         greedy_str = "greedy" if greedy else "non-greedy"
         self.logger.debug("t:%s a:%s %s", t, a, greedy_str)
         return a
